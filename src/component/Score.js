fe893e5dbc798e730def81b91f226415717e4d79
jinjishot <jinjishot@gmail.com>
2022-09-30 11:18:35 +0700

    first commit

--- src/component/Score.js
+++ src/component/Score.js
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { DataContext } from "../App";
+import QuestionsData from "../data/QuestionsData";
+
 const Score = () => {
+    const {score, setAppState, setScore} = useContext(DataContext)
+
+    const reStartApp = () => {
+        setScore(0)
+        setAppState("menu")
+    }
+
     return(
         <div className="score">
-            <h1>Score Component</h1>
+            <h1>Your Score</h1>
+            <h2>{score} / {QuestionsData.length}</h2>
+            <button onClick={reStartApp}>Back to Menu</button>
         </div>
     )
 }
